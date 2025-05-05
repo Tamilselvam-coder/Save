397b555a8219f753a8b70b7419850ee89347451a
Tamilselvam-coder <116172757+Tamilselvam-coder@users.noreply.github.com>
2025-05-05 15:50:35 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -1,16 +1,3 @@
-# ---------------------------------------------------
-# File Name: __main__.py
-# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
-#              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
-# Created: 2025-01-11
-# Last Modified: 2025-01-11
-# Version: 2.0.5
-# License: MIT License
-# ---------------------------------------------------
 
 import asyncio
 import importlib
@@ -41,9 +28,9 @@ async def devggn_boot():
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
 👨‍💻 Author: Gagan
-🌐 GitHub: https://github.com/devgaganin/
-📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
+🌐 GitHub: https://github.com/tamilrushertar/
+📬 Telegram: https://t.me/botsworldtar
+▶️ YouTube: https://youtube.com/@akkaefx
 🗓️ Created: 2025-01-11
 🔄 Last Modified: 2025-01-11
 🛠️ Version: 2.0.5
